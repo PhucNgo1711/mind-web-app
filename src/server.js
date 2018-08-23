0a69f81539f091dee212cd006f054b53e36ced45
PhucNgo1711 <phucnmrot@gmail.com>
2018-08-24 03:06:27 +0700

    Change package.json for Heroku

--- src/server.js
+++ src/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-
 const port = process.env.PORT || 5000;
 
+// app.get('/ping', function (req, res) {
+//   res.send({ express: 'pong' });
+// });
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
-
-app.get('/ping', function (req, res) {
-  res.send({ express: 'pong' });
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 app.listen(port, () => console.log(`Listening on port ${port}`));
